591e78986275eab912a203d43d5b74405cb62e44
Hiba Nafeesath <74949778+hibanafeesath@users.noreply.github.com>
2023-04-25 10:55:56 +0530

    Delete ECommerce directory

--- ECommerce/ECommerce/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ECommerce.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ECommerce/ECommerce/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECommerce.WebApiApplication" Language="C#" %>
